460ae7348796b5d367cc09de65b1e2e2f8889928
Anil Kumar K K <akumarkk@outlook.com>
2023-07-21 21:14:56 +0000

    api

---
+++ DishesAPI/DishesControlleAPI/Controllers/DishesController.cs
@@ -0,0 +1,26 @@
+﻿using DishesControlleAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DishesControlleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DishesController : ControllerBase
+    {
+        private readonly DishesApiContext dishesApiContext;
+
+        public DishesController(DishesApiContext dishesApiContext)
+        {
+            this.dishesApiContext = dishesApiContext;
+            
+        }
+
+        [HttpGet] 
+        public async Task<ActionResult<List<Dish>>> Dishes()
+        {
+            return Ok(await dishesApiContext.Dishes.ToListAsync());
+        }
+        
+    }
+}
